6b082c75b073960bf849cad5ac5df9a393b30749
selemilka <selemilka@gmail.com>
2022-04-24 19:29:24 +0300

    init commit

---
+++ src/test/kotlin/com/sdfomin/businesscard/BusinesscardApplicationTests.kt
@@ -0,0 +1,14 @@
+package com.sdfomin.businesscard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BusinesscardApplicationTests {
+
+	@Test
+	fun contextLoads() {
+
+	}
+
+}
